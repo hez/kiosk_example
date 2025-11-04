f35f5c39d7eba311183b77b821e9ddf0115d57b2
Hez Ronningen <code@hez.ca>
2025-11-04 10:37:47 -0800

    unpochoifying the config and application children
    - introduced a config for target much like how the nerves phoenix example does it
    - renamed the phoenix config/runtime.exs as it contains app crashing behaviour by default

--- lib/kiosk_example/application.ex
+++ lib/kiosk_example/application.ex
@@ -12,7 +12,16 @@ defmodule KioskExample.Application do
         # Children for all targets
         # Starts a worker by calling: KioskExample.Worker.start_link(arg)
         # {KioskExample.Worker, arg},
-      ] ++ phoenix_children() ++ children()
+        KioskExampleWeb.Telemetry,
+        {DNSCluster, query: Application.get_env(:kiosk_example, :dns_cluster_query) || :ignore},
+        {Phoenix.PubSub, name: KioskExample.PubSub},
+        # Start the Finch HTTP client for sending emails
+        {Finch, name: KioskExample.Finch},
+        # Start a worker by calling: KioskExample.Worker.start_link(arg)
+        # {KioskExample.Worker, arg},
+        # Start to serve requests, typically the last entry
+        KioskExampleWeb.Endpoint
+      ] ++ children(target())
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -21,49 +30,36 @@ defmodule KioskExample.Application do
   end
 
   # List all child processes to be supervised
-  if Mix.target() == :host do
-    defp children() do
-      [
-        # Children that only run on the host
-        # Starts a worker by calling: KioskExample.Worker.start_link(arg)
-        # {KioskExample.Worker, arg},
-      ]
-    end
-  else
-    defp children() do
-      # NOTE: work around to stop watchers on targets
-      Application.get_env(:kiosk_example, KioskExampleWeb.Endpoint)
-      |> Keyword.put(:watchers, [])
-      |> then(&Application.put_env(:kiosk_example, KioskExampleWeb.Endpoint, &1))
-
-      start_node()
-
-      [
-        # Children for all targets except host
-        # Starts a worker by calling: KioskExample.Worker.start_link(arg)
-        # {KioskExample.Worker, arg},
-        {KioskExample.DisplaySupervisor, []}
-      ]
-    end
-
-    defp start_node() do
-      {_, 0} = System.cmd("epmd", ~w"-daemon")
-      {:ok, _pid} = Node.start(:"kiosk_example@nerves.local")
-      Node.set_cookie(Application.get_env(:mix_tasks_upload_hotswap, :cookie))
-    end
-  end
-
-  defp phoenix_children() do
+  def children(:host) do
     [
-      KioskExampleWeb.Telemetry,
-      {DNSCluster, query: Application.get_env(:kiosk_example, :dns_cluster_query) || :ignore},
-      {Phoenix.PubSub, name: KioskExample.PubSub},
-      # Start the Finch HTTP client for sending emails
-      {Finch, name: KioskExample.Finch},
-      # Start a worker by calling: KioskExample.Worker.start_link(arg)
+      # Children that only run on the host
+      # Starts a worker by calling: KioskExample.Worker.start_link(arg)
       # {KioskExample.Worker, arg},
-      # Start to serve requests, typically the last entry
-      KioskExampleWeb.Endpoint
     ]
   end
+
+  def children(tar) do
+    dbg(tar)
+    # NOTE: work around to stop watchers on targets
+    Application.get_env(:kiosk_example, KioskExampleWeb.Endpoint)
+    |> Keyword.put(:watchers, [])
+    |> then(&Application.put_env(:kiosk_example, KioskExampleWeb.Endpoint, &1))
+
+    start_node()
+
+    [
+      # Children for all targets except host
+      # Starts a worker by calling: KioskExample.Worker.start_link(arg)
+      # {KioskExample.Worker, arg},
+      {KioskExample.DisplaySupervisor, []}
+    ]
+  end
+
+  defp start_node() do
+    {_, 0} = System.cmd("epmd", ~w"-daemon")
+    {:ok, _pid} = Node.start(:"kiosk_example@nerves.local")
+    Node.set_cookie(Application.get_env(:mix_tasks_upload_hotswap, :cookie))
+  end
+
+  def target, do: Application.get_env(:kiosk_example, :target)
 end
